a3f1fea1e611167ebf1ac3bde79fe621a75cc2c7
Nic Chan <channicoleelise@gmail.com>
2024-09-10 15:25:22 +0800

    Move off of legacy endpoint

--- src/components/FooterWindows.svelte
+++ src/components/FooterWindows.svelte
@@ -42,19 +42,21 @@
 	}
 </script>
 
-<ul class="windows__list" aria-label="Windows list">
-	{#each $orderedWindows as window}
-		<li class="windows__item">
-			<button
-				class="windows__button button--ui"
-				aria-expanded={window.isMinimized ? false : true}
-				aria-controls={window.id}
-				on:click={() => onWindowClick(window)}>
-				<span class="button--ui__content"> {window.name}</span>
-			</button>
-		</li>
-	{/each}
-</ul>
+<div class="windows">
+	<ul class="windows__list" aria-label="Windows list">
+		{#each $orderedWindows as window}
+			<li class="windows__item">
+				<button
+					class="windows__button button--ui"
+					aria-expanded={window.isMinimized ? false : true}
+					aria-controls={window.id}
+					on:click={() => onWindowClick(window)}>
+					<span class="button--ui__content"> {window.name}</span>
+				</button>
+			</li>
+		{/each}
+	</ul>
+</div>
 
 <style lang="scss">
 	.windows__list {
